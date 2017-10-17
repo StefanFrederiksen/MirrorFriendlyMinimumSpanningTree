9da6d7764ffe3ed55b2ebd34ce18428bf8313526
StefanFrederiksen <stefanlemvigfrederiksen+github@gmail.com>
2017-10-17 14:05:46 +0200

    Added visual studio projects

---
+++ Application/App.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
